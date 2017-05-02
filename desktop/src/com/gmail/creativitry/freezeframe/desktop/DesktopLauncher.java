d435adc1d960710c6b240ccd1da86818eaea2a4a
Tiger <tiger.c.zhang@gmail.com>
2017-05-02 11:07:46 -0500

    Initial commit

---
+++ desktop/src/com/gmail/creativitry/freezeframe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gmail.creativitry.freezeframe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gmail.creativitry.freezeframe.FreezeFrame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FreezeFrame(), config);
+	}
+}
