7bc8182ba9e56835f8f37091cebf8e19ff7c528d
Tiger <tiger.c.zhang@gmail.com>
2017-05-16 12:31:11 -0500

    fixed magic numbers

--- core/src/com/gmail/creativitry/freezeframe/moveable/bullet/AbstractPolarBullet.java
+++ core/src/com/gmail/creativitry/freezeframe/moveable/bullet/AbstractPolarBullet.java
@@ -14,7 +14,7 @@ public abstract class AbstractPolarBullet extends AbstractBullet
 {
 	private float angle;
 	private float radVel;
-	private float final ANGLES_IN_CIRCLE  = 360;
+	private final float DEGREES_IN_CIRCLE = 360;
 	
 	private float distFromCenter;
 	
@@ -26,9 +26,9 @@ public abstract class AbstractPolarBullet extends AbstractBullet
 	public void setAngle(float angle)
 	{
 		while (angle < 0)
-			angle += ANGLES_IN_CIRCLE;
+			angle += DEGREES_IN_CIRCLE;
 		while (angle >= 360)
-			angle -= ANGLES_IN_CIRCLE;
+			angle -= DEGREES_IN_CIRCLE;
 		this.angle = angle;
 	}
 	
